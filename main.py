75ad7ce8a9a95c21ae089f3a679893e2ec702731
eda235648-jpg <eda235648@gmail.com>
2026-06-17 18:38:57 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🧪 Kadim Simyacının Sırrı")
+st.write("Laboratuvarda kilitli kaldın! Kurtulmak için güvenlik ekipmanlarını seçmelisin.")
+
+if st.button("Önlük, Gözlük ve Eldivenimi takıyorum!"):
+    st.success("Güvenlik sağlandı! Şimdi deney masasına geçebilirsin.")
